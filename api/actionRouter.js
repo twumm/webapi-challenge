83e14a44709c77721eaa871ed8463371a9e83e7b
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-12 08:55:41 +0000

    feature: Create middleware to validate action id

--- api/actionRouter.js
+++ api/actionRouter.js
@@ -4,7 +4,15 @@ const router = express.Router();
 const actionDb = require('../data/helpers/actionModel');
 
 router.get('/', async (req, res, next) => {
-
+  try {
+    const actions = await actionDb.getAllActions();
+    res
+      .status(200)
+      .json(actions);
+  }
+  catch (error) {
+    next(error);
+  }
 })
 
 router.get('/:id', async (req, res, next) => {
@@ -18,5 +26,19 @@ router.delete('/:id', async (req, res, next) => {
 router.put('/:id', async (req, res, next) => {
   
 })
+
+// custom middlewares
+async function validateActionId(req, res, next) {
+  const { id } = req.params;
+  const action = await actionDb.get(id);
+  if (action) {
+    req.action = action;
+    next()
+  } else {
+    res
+      .status(400)
+      .json({ message: "Invalid action id" });
+  }
+}
 
 module.exports = router;
