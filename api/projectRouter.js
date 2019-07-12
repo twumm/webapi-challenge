2a807e98f873f41593c94238cfeb95b9e30b13ee
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-12 07:34:29 +0000

    feature: Create middleware to validate project id and check if body of request has required fields for creating a project

--- api/projectRouter.js
+++ api/projectRouter.js
@@ -3,4 +3,29 @@ const express = require('express');
 const router = express.Router();
 const projectDb = require('../data/helpers/projectModel');
 
+// custom middlewares
+async function validateProjectId(req, res, next) {
+  const { id } = req.params;
+  if (isNaN(Number(id))) {
+    res.status(400).json({ message: 'User id must be a number' })
+  }
+  const project = await projectDb.getById(id);
+  if (project) {
+    req.project = project;
+    next();
+  } else {
+    res.status(400).json({ message: "Invalid project id" });
+  }
+}
+
+function validationProjectContent(req, res, next) {
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    res.status(400).json({ message: "Missing project data" });
+  } else if (!req.body.name || !req.body.description) {
+    res.status(400).json({ message: "Missing required *name* and *description* fields" });
+  } else {
+    next();
+  }
+}
+
 module.exports = router;
