60c77b14b2febd193da52e8065206d5f0e07e6b7
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-12 06:56:11 +0000

    feature: Create server with express Create middleware to log all requests to the console Create error handler middleware Create demo route to test server implementation

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+
+server.use(express.json());
+server.use(logger);
+
+server.get('/', (req, res) => {
+  res.send('Projects and actions!');
+})
+
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} request from ${req.url}}`
+  );
+  next();
+};
+
+function errorHandler(error, req, res, next) {
+  res.status(500).json({ message: "There was an error" })
+}
+
+server.use(errorHandler);
+
+module.exports = server;
