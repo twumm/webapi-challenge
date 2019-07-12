4394c94317fe438ed286a6e71f1024c61eaa2054
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-12 08:46:15 +0000

    feature: Create require basic http methods

--- api/actionRouter.js
+++ api/actionRouter.js
@@ -3,4 +3,20 @@ const express = require('express');
 const router = express.Router();
 const actionDb = require('../data/helpers/actionModel');
 
+router.get('/', async (req, res, next) => {
+
+})
+
+router.get('/:id', async (req, res, next) => {
+  
+})
+
+router.delete('/:id', async (req, res, next) => {
+  
+})
+
+router.put('/:id', async (req, res, next) => {
+  
+})
+
 module.exports = router;
