88ad082e2369b3bb4907944dfecec59cf25be3dd
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-13 03:27:31 +0000

    feature: Pass down props

--- client-webapi-projects/src/components/projects/ProjectList.js
+++ client-webapi-projects/src/components/projects/ProjectList.js
@@ -3,15 +3,20 @@ import React from 'react'
 import Project from './Project';
 import ProjectForm from './ProjectForm';
 
-const ProjectList = ({ projects }) => {
+const ProjectList = ({ projects, error, loading, getProject, createProject, deleteProject, updateProject }) => {
   return (
     <div>
+      { error && <p>oops! Something bad happened!</p> }
+      { loading && <p>Wait for it!</p> }
       {
         projects.map(project => (
           <Project
+            key={project.id}
             name={project.name}
             description={project.description}
             completed={project.completed}
+            deleteProject={deleteProject}
+            updateProject={updateProject}
           />
         ))
       }
